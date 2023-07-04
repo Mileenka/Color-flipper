4644b87259f14b4663c2025e5b870f64105428ca
Milena Wasowicz <milena.wasowicz@gmail.com>
2023-07-04 11:17:01 +0200

    refactoring

--- js/hex.js
+++ js/hex.js
@@ -1,23 +1,30 @@
-/* Variables */
+'use strict'
+
+import getRandomNumber from './utils/getRandomNumber.js'
+
+/* DOM Elements */
 
 const btn = document.getElementById('btn');
 
 const color = document.querySelector('.color-span');
 
+/* Variables */
+
 const hex = ['17172B', '191919', '5E0A5E', '430A13'];
 
-/* Function */
+const body = document.body;
 
-const getRandomNumber = () => {
-    return Math.floor(Math.random() * hex.length);
+const hexLength = hex.length;
+
+/* Handlers */
+
+const changeBgHexColor = () => {
+    let hexColor = '#';
+    hexColor += hex[getRandomNumber(hex.length)];
+    body.style.background = hexColor;
+    color.textContent = hexColor;
 }
 
 /* Events */
 
-btn.addEventListener('click', () => {
-    let hexColor = '#';
-    hexColor += hex[getRandomNumber()];
-    colors = hexColor;
-    document.body.style.background = hexColor;
-    color.textContent = hexColor;
-});
+btn.addEventListener('click', changeBgHexColor);
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 'use strict';
+import getRandomNumber from './utils/getRandomNumber.js'
 
-/* Variables */
+
+/* DOM Element */
 
 const btn = document.getElementById('btn');
 
 const color = document.querySelector(".color-span");
+
+/* Variables */
 
 const colors = ["rgb(23, 23, 43)", "rgb(25, 25, 25)", "rgb(94, 10, 94)",
     "rgb(67, 10, 19)"];
@@ -12,19 +16,22 @@ const colors = ["rgb(23, 23, 43)", "rgb(25, 25, 25)", "rgb(94, 10, 94)",
 const borderColors = ["rgb(12, 13, 23)", "rgb(00, 15, 05)", "rgb(64, 00, 74)",
     "rgb(37, 00, 09)"];
 
-/* Functions */
+const colorsLength = colors.length;
 
-const getRandomNumber = () => {
-    return Math.floor(Math.random() * colors.length);
+const body = document.body;
+
+/* Handlers */
+
+const changeColors = () => {
+    // get random number between 0 - 3
+    const randomNumber = getRandomNumber(colorsLength);
+    const randomColors = colors[randomNumber];
+    body.style.backgroundColor = randomColors;
+    btn.style.backgroundColor = randomColors;
+    color.textContent = randomColors;
+    btn.style.border = `3px solid ${borderColors[randomNumber]}`;
 }
 
 /* Events */
 
-btn.addEventListener('click', () => {
-    // get random number between 0 - 3
-    const randomNumber = getRandomNumber();
-    document.body.style.backgroundColor = colors[randomNumber];
-    btn.style.backgroundColor = colors[randomNumber];
-    btn.style.border = `3px solid ${borderColors[randomNumber]}`;
-    color.textContent = colors[randomNumber];
-});
+btn.addEventListener('click', changeColors);
